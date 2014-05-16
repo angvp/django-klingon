945db71937d5e4bf80f6d222a9e182a350c63438
Angel Velasquez <angel@routeatlas.com>
2014-05-15 21:33:56 -0300

    Initial commit

---
+++ README.rst
@@ -0,0 +1,16 @@
+klingon
+========================
+
+Welcome to the documentation for django-klingon!
+
+
+Running the Tests
+------------------------------------
+
+You can run the tests with via::
+
+    python setup.py test
+
+or::
+
+    python runtests.py
---
+++ runtests.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import sys
+
+import django
+from django.conf import settings
+
+
+if not settings.configured:
+    settings.configure(
+        DATABASES={
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': ':memory:',
+            }
+        },
+        INSTALLED_APPS=(
+            'klingon',
+        ),
+        SITE_ID=1,
+        SECRET_KEY='this-is-just-for-tests-so-not-that-secret',
+    )
+
+
+from django.test.utils import get_runner
+
+
+def runtests():
+    if hasattr(django, 'setup'):
+        django.setup()
+    apps = sys.argv[1:] or ['klingon', ]
+    TestRunner = get_runner(settings)
+    test_runner = TestRunner(verbosity=1, interactive=True, failfast=False)
+    failures = test_runner.run_tests(apps)
+    sys.exit(failures)
+
+
+if __name__ == '__main__':
+    runtests()
