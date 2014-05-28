4b2c7e71226eeefe577648f59b3a214179c34881
Angel Velasquez <angel@routeatlas.com>
2014-05-28 06:49:55 -0300

    Add description, preparing for the first release

--- klingon/__init__.py
+++ klingon/__init__.py
@@ -1,3 +1,6 @@
-"<Include a description of your project>"
+"""
+django-klingon is an attempt to make django model translations suck but with
+no integrations pain in your app!
+"""
 
-__version__ = '0.0.0'
+__version__ = '0.0.1'
