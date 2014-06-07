b353d8f93a38daac597bc518ea65ff232163e279
Rafael Capdevielle <capdevielle@gmail.com>
2014-06-07 16:33:45 -0300

    Completed translatemodels command. Added more unit tests

--- klingon/management/commands/translatemodels.py
+++ klingon/management/commands/translatemodels.py
@@ -1,13 +1,19 @@
 from django.core.management.base import BaseCommand, CommandError
+from django.db.models.loading import get_model
+
 
 class Command(BaseCommand):
-    args = '<model_name model_name ...>'
-    help = 'Create translation for specific models <app>.<model_name>'
+    args = '<app.Model1 app.Model2 ...>'
+    help = 'Create translation for specific models <app>.<model_name>' \
+        ' \n Example: \n $ python manage.py translatemodels testapp.Book'
 
     def handle(self, *args, **options):
         for model_name in args:
             try:
-                pass
+                app, name = model_name.split('.')
+                model = get_model(app, name)
+                for obj in model.objects.all():
+                    obj.translate()
             except Exception as e:
                 raise CommandError('Error, can not translate model "%s". %s' % (model_name, e))
             else:
--- klingon/tests/test_commands.py
+++ klingon/tests/test_commands.py
@@ -2,6 +2,7 @@ import datetime
 from django.conf import settings
 from django.core.cache import cache
 from django.core.management import call_command
+from django.core.management.base import CommandError
 from django.test import TestCase
 from testapp.models import Book
 from klingon.models import Translation
@@ -21,11 +22,26 @@ class CommandsTestCase(TestCase):
         # Remove cache after each test
         cache.clear()
 
-    def test_translate_models_command(self):
-        args = ['testapp.Book']
+    def _test_translate_models_command(self, args):
+        args = args
         opts = {}
         call_command('translatemodels', *args, **opts)
+
+    def test_translate_models_command_success(self):
+        self._test_translate_models_command(['testapp.Book'])
         self.assertEquals(
             len(Translation.objects.all()),
             len(settings.LANGUAGES*len(self.book.translatable_fields)),
         )
+
+    def test_translate_models_command_empty(self):
+        self._test_translate_models_command([])
+        self.assertEquals(len(Translation.objects.all()), 0)
+
+    def test_translate_models_command_wrong_args(self):
+        self.assertRaises(
+            CommandError,
+            self._test_translate_models_command,
+            ['wrong.argument']
+        )
+
