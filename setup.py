5ab7640c45ecb530fa24425b6236e8cb6c61066c
Angel Velasquez <angel@routeatlas.com>
2014-12-08 06:35:50 -0300

    Add python3 support claiming on setup.py, yay!

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
         'Framework :: Django',
         'Development Status :: 4 - Beta',
         'Operating System :: OS Independent',
