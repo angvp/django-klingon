161ae3bd22fd7c166a85f35df987bab6b30f9b8d
Sultan Imanhodjaev <sultan.imanhodjaev@gmail.com>
2016-01-08 19:25:52 +0600

    update(compat) add more support from Django v1.5 to v1.9

--- klingon/compat.py
+++ klingon/compat.py
@@ -1,19 +1,23 @@
 from __future__ import print_function, unicode_literals, with_statement, division
 from django import VERSION
 
+version = VERSION[1]
 
-V18 = VERSION[1] == 8
-V19 = VERSION[1] == 9
+V15 = 5
+V16 = 6
+V17 = 7
+V18 = 8
+V19 = 9
 
-if V18:
+
+if version in [V15, V16]:
     from django.db.models.loading import get_model
-    from django.contrib.contenttypes.generic import GenericForeignKey
-    from django.contrib.contenttypes.generic import GenericTabularInline
-elif V19:
+    from django.contrib.contenttypes.generic import GenericForeignKey, GenericTabularInline
+
+if version in [V17, V18, V19]:
     from django.apps.registry import apps
     from django.contrib.contenttypes.fields import GenericForeignKey
     from django.contrib.contenttypes.admin import GenericTabularInline
-
     get_model = apps.get_model
 
 
