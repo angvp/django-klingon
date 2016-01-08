2ad714df3c4f1d45e6fed908669334ac2d84bfa6
Sultan Imanhodjaev <sultan.imanhodjaev@gmail.com>
2016-01-08 14:14:30 +0600

    add(compat file) add compat.py file to support backward compatibility from Django v1.8

---
+++ klingon/compat.py
@@ -0,0 +1,20 @@
+from __future__ import print_function, unicode_literals, with_statement, division
+from django import VERSION
+
+
+V18 = VERSION[1] == 8
+V19 = VERSION[1] == 9
+
+if V18:
+    from django.db.models.loading import get_model
+    from django.contrib.contenttypes.generic import GenericForeignKey
+    from django.contrib.contenttypes.generic import GenericTabularInline
+elif V19:
+    from django.apps.registry import apps
+    from django.contrib.contenttypes.fields import GenericForeignKey
+    from django.contrib.contenttypes.admin import GenericTabularInline
+
+    get_model = apps.get_model
+
+
+__all__ = ['get_model', 'GenericForeignKey', GenericTabularInline]
